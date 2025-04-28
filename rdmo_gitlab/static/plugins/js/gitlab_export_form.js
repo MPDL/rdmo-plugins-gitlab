9f5735fe07462c76a56bec480ca7435cf31a2a24
Laura Bahamon <bahamon@mpdl.mpg.de>
2025-04-28 11:12:39 +0200

    updated

--- rdmo_gitlab/static/plugins/js/gitlab_export_form.js
+++ rdmo_gitlab/static/plugins/js/gitlab_export_form.js
@@ -3,7 +3,7 @@ function toggleRepoFields(checkbox_id, checked_collection_class, unchecked_colle
     var checkedCollection = document.getElementsByClassName(checked_collection_class);
     var uncheckedCollection = document.getElementsByClassName(unchecked_collection_class);
     var submitButton = document.getElementsByClassName('btn btn-primary');
-    // console.log(`checked_submit_value: ${checked_submit_value}`)
+
     if (checkBox.checked == true){
         checkedCollection[0].style.display = 'block';
         uncheckedCollection[0].style.display = 'none';
@@ -16,7 +16,6 @@ function toggleRepoFields(checkbox_id, checked_collection_class, unchecked_colle
 }
 
 function select_all_exports(choice_count) {
-    // console.log('select_all_exports()')
     var checkBox = document.getElementById('id_all_exports');
 
     for (let i=0; i<choice_count; i++) {
@@ -32,8 +31,6 @@ function select_all_exports(choice_count) {
 }
 
 function toggle_option_attributes_visibility(element) {
-    // console.log('show_file_path()')
-    // console.log(element)
     const index = element.id.replace('id_exports_', '')
     
     var file_path_span = document.getElementById(`id_exports_file_path_${index}`);
@@ -44,14 +41,11 @@ function toggle_option_attributes_visibility(element) {
 }
 
 function hide_check_message(element) {
-    // console.log('hide_check_message()')
-    // console.log(element)
     const index = element.id.replace('id_exports_text_', '')
 
     var duration = 1000;
     clearTimeout(element._timer);
     element._timer = setTimeout(()=>{
-        // console.log('   hiding check message')
         var check_message_span = document.getElementById(`id_exports_check_message_${index}`);
         check_message_span.style.display = 'none';
     }, duration);
