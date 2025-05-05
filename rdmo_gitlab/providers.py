9fcdbdda23078c7eff33da984f10e06e1ca2acc1
Laura Bahamon <bahamon@mpdl.mpg.de>
2025-05-05 12:44:00 +0200

    changed repo charfield to choicefield + added js to branch field

--- rdmo_gitlab/providers.py
+++ rdmo_gitlab/providers.py
@@ -5,6 +5,7 @@ import requests
 
 from urllib.parse import quote
 
+from django import forms
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import redirect, render
 from django.utils.translation import gettext_lazy as _
@@ -59,14 +60,14 @@ class GitLabExportProvider(GitLabProviderMixin, MAUSExport):
         context = {
             'new_repo_name_display': 'none',
             'repo_display': 'block',
-            'form': GitLabExportForm(export_choices=self.export_choices),
+            'form': self.get_form(self.request, GitLabExportForm, export_choices=self.export_choices), # GitLabExportForm(export_choices=self.export_choices),
             'source_title': self.gitlab_url,
             'submit_label': _('Proceed')
         }
         return render(self.request, 'plugins/gitlab_export_form.html', context, status=200)
 
     def submit(self):
-        form = GitLabExportForm(self.request.POST, export_choices=self.export_choices)
+        form = self.get_form(self.request, GitLabExportForm, self.request.POST, export_choices=self.export_choices) # GitLabExportForm(self.request.POST, export_choices=self.export_choices)
 
         if 'cancel' in self.request.POST:
             if self.project is None:
@@ -80,16 +81,24 @@ class GitLabExportProvider(GitLabProviderMixin, MAUSExport):
             # 1. Check file paths to warn user if repo files will be overwritten
             choices_to_update = self.get_from_session(self.request, 'gitlab_export_choices_to_update')
             if not new_repo and choices_to_update is None:
-                choices_to_update, checked_export_choices = self.check_file_paths(
+                choices_to_update, checked_export_choices, checked_branch = self.check_file_paths(
                     form.cleaned_data['exports'], 
                     form.cleaned_data['repo'], 
                     form.cleaned_data['branch']
                 )
                 self.store_in_session(self.request, 'gitlab_export_choices_to_update', choices_to_update)
                 self.store_in_session(self.request, 'gitlab_checked_export_choices', checked_export_choices)
+                self.store_in_session(self.request, 'gitlab_checked_branch', checked_branch)
                 
                 selected_choices = [c for c in self.export_choices if c[1][1] in choices_to_update.keys()]
-                form = GitLabExportForm(self.request.POST, export_choices=selected_choices, export_choices_to_update=choices_to_update)
+                form = self.get_form(
+                    self.request, 
+                    GitLabExportForm, 
+                    self.request.POST, 
+                    export_choices=selected_choices, 
+                    export_choices_to_update=choices_to_update
+                ) 
+                # GitLabExportForm(self.request.POST, export_choices=selected_choices, export_choices_to_update=choices_to_update)
                 context = {
                     'new_repo_name_display': 'none',
                     'repo_display': 'block',
@@ -120,29 +129,40 @@ class GitLabExportProvider(GitLabProviderMixin, MAUSExport):
         return render(self.request, 'plugins/gitlab_export_form.html', context, status=200)
     
     def get_file_metadata(self, request, url):
+        print('get_file_metadata()')
         access_token = self.get_from_session(request, 'access_token')
+        print(f'    access_token: {access_token}')
         if access_token:
-            headers = self.get_authorization_headers(access_token)
-            response = requests.head(url, headers=headers)
-            if response: return response
+            response = requests.head(url, headers=self.get_authorization_headers(access_token))
+            if response: 
+                print(f'    response: ')
+                try:
+                    print(f'    {response.status_code}')
+                except:
+                    print(f'    cannot print status code')
+                return response
         return None
 
     def check_file_paths(self, exports, repo, branch):
+        print('check_file_paths()')
         choices_to_update = {}
         for e in exports:
             choice_key, file_path = e.split(',')
+            print(f'    choice_key: {choice_key}, file_path: {file_path}')
             url = '{api_url}/projects/{repo}/repository/files/{path}?ref={ref}'.format(
                 api_url=self.api_url,
-                repo=quote(repo.removesuffix('/').replace('https://gitlab.com/', ''), safe=''),
+                repo=quote(repo.replace(self.gitlab_url, '').strip('/'), safe=''),
                 path=quote(file_path, safe=''),
                 ref=quote(branch, safe='')
             )
+            print(f'    url: {url}')
 
             response = self.get_file_metadata(self.request, url)
+            # print(f'    response: {response.json()}')
             choice_in_repo = True if response is not None and response.status_code == 200 else False
             choices_to_update[choice_key] = choice_in_repo
 
-        return choices_to_update, exports
+        return choices_to_update, exports, branch
     
     def render_export(self, choice_key):
         smp_exports = getattr(self, 'smp_exports', None)
@@ -174,15 +194,18 @@ class GitLabExportProvider(GitLabProviderMixin, MAUSExport):
         actions = []
         processed_exports = []
 
-        checked_export_choices = self.get_from_session(self.request, 'gitlab_checked_export_choices')
+        checked_export_choices = self.pop_from_session(self.request, 'gitlab_checked_export_choices')
+        checked_branch = self.pop_from_session(self.request, 'gitlab_checked_branch')
         new_repo = form_data['new_repo']
-        
+        branch = 'main' if new_repo else form_data['branch']
+
         exports = form_data['exports']
         for e in exports:
             choice_key, file_path = e.split(',')
             initial_file_path = file_path if new_repo else next((exp.split(',')[1] for exp in checked_export_choices if exp.split(',')[0] == choice_key), file_path)
-            if file_path != initial_file_path:
-                new_choices_to_update, __ = self.check_file_paths([e], form_data['repo'], form_data['branch'])
+            initial_branch = 'main' if new_repo else checked_branch
+            if file_path != initial_file_path or branch != initial_branch:
+                new_choices_to_update, __, ___ = self.check_file_paths([e], form_data['repo'], branch)
                 choices_to_update[choice_key] = new_choices_to_update[choice_key]
                 if new_choices_to_update[choice_key] == True and not update_without_warning:
                     processed_exports.append({
@@ -225,14 +248,14 @@ class GitLabExportProvider(GitLabProviderMixin, MAUSExport):
 
         self.store_in_session(self.request, 'gitlab_processed_exports', processed_exports)
 
-        repo = 'repo_placeholder' if new_repo else quote(form_data['repo'].removesuffix('/').replace('https://gitlab.com/', ''), safe='')
+        repo = 'repo_placeholder' if new_repo else quote(form_data['repo'].replace(self.gitlab_url, '').strip('/'), safe='')
         url = '{api_url}/projects/{repo}/repository/commits'.format(
                 api_url=self.api_url,
                 repo=repo,
             )
 
         request_data = {
-            'branch': quote(form_data['branch'], safe=''),
+            'branch': quote(branch, safe=''),
             'commit_message': quote(form_data['commit_message'], safe=' '),
             'actions': actions
         }
@@ -271,11 +294,26 @@ class GitLabIssueProvider(GitLabProviderMixin, OauthIssueProvider):
         return _(f'This integration allows the creation of issues in arbitrary repositories on {self.gitlab_url}. '
                  'The upload of attachments is not supported by GitLab.')
 
+    _fields = {
+        'repo_url': {
+            'key': 'repo_url',
+            'placeholder': 'placeholder',
+            'help': _('The URL of the GitLab repository to send issues to.')
+        },
+        'secret': {
+            'key': 'secret',
+            'placeholder': 'Secret (random) string',
+            'help': _('The secret for a GitLab webhook to close a task (optional).'),
+            'required': False,
+            'secret': True
+        }
+    }
+
     def get_post_url(self, request, issue, integration, subject, message, attachments):
         repo_url = integration.get_option_value('repo_url')
         if repo_url:
-            repo = repo_url.removesuffix('/').replace(self.gitlab_url, '')
-            return '{}/api/v4/projects/{}/issues'.format(self.gitlab_url, quote(repo, safe=''))
+            repo = quote(repo_url.replace(self.gitlab_url, '').strip('/'), safe='')
+            return '{}/api/v4/projects/{}/issues'.format(self.gitlab_url, repo)
 
     def get_post_data(self, request, issue, integration, subject, message, attachments):
         return {
@@ -314,35 +352,52 @@ class GitLabIssueProvider(GitLabProviderMixin, OauthIssueProvider):
                 return HttpResponse(e, status=400)
 
         raise Http404
-
+    
     @property
     def fields(self):
-        return [
-            {
-                'key': 'repo_url',
-                'placeholder': f'{self.gitlab_url}/username/repo',
-                'help': _('The URL of the GitLab repository to send issues to.')
-            },
-            {
-                'key': 'secret',
-                'placeholder': 'Secret (random) string',
-                'help': _('The secret for a GitLab webhook to close a task (optional).'),
-                'required': False,
-                'secret': True
-            }
-        ]
+        return self._fields.values()
+    
+    @fields.setter
+    def fields(self, new_fields):        
+        if isinstance(new_fields, dict):
+            for k, v in new_fields.items():
+                self._fields[k] = v
 
+    def integration_setup(self, request):
+        redirect_url = request.build_absolute_uri()
+        self.store_in_session(request, 'redirect_url', redirect_url)
+        minimum_repo_access_level = 15 # planner
+        repo_choices, repo_help_text = self.get_repo_form_field_data(request, minimum_repo_access_level)
 
+        gitlab_app_repo_url = {**self._fields['repo_url']}
+        if repo_choices is not None:
+            gitlab_app_repo_url['widget'] = forms.RadioSelect(choices=repo_choices)
+            
+        if repo_help_text is not None:
+            gitlab_app_repo_url['help'] = repo_help_text
+
+        self.fields = {'repo_url': gitlab_app_repo_url}
+
+    
 class GitLabImport(GitLabProviderMixin, RDMOXMLImport):
 
     def render(self):
-        return render(self.request, 'projects/project_import_form.html', {
+        access_token = self.validate_access_token(self.request, self.get_from_session(self.request, 'access_token'))
+        if access_token is None:
+            redirect_url = self.request.build_absolute_uri()
+            self.store_in_session(self.request, 'redirect_url', redirect_url)
+            return self.authorize(self.request)
+        
+        context = {
             'source_title': self.gitlab_url,
-            'form': GitLabImportForm()
-        }, status=200)
+            'repo_display': 'block',
+            'other_repo_display': 'none',
+            'form': self.get_form(self.request, GitLabImportForm)
+        }
+        return render(self.request, 'plugins/gitlab_import_form.html', context, status=200)
 
     def submit(self):
-        form = GitLabImportForm(self.request.POST)
+        form = self.get_form(self.request, GitLabImportForm, self.request.POST)
 
         if 'cancel' in self.request.POST:
             if self.project is None:
@@ -353,20 +408,38 @@ class GitLabImport(GitLabProviderMixin, RDMOXMLImport):
         if form.is_valid():
             self.request.session['import_source_title'] = form.cleaned_data['path']
 
-            url = '{api_url}/projects/{repo}/repository/files/{path}?ref={ref}'.format(
-                api_url=self.api_url,
-                repo=quote(form.cleaned_data['repo'].removesuffix('/').replace('https://gitlab.com/', ''), safe=''),
-                path=quote(form.cleaned_data['path'], safe=''),
-                ref=quote(form.cleaned_data['ref'], safe='')
-            )
+            url = self.process_form_data(form.cleaned_data)
 
             return self.make_request(self.request, 'get', url)
 
-        return render(self.request, 'projects/project_import_form.html', {
+        other_repo_check = True if 'other_repo_check' in form.data else False
+        repo_display = 'none' if other_repo_check else 'block'
+        other_repo_display = 'block' if other_repo_check else 'none'
+        context = {
             'source_title': self.gitlab_url,
+            'repo_display': repo_display,
+            'other_repo_display': other_repo_display,
             'form': form
-        }, status=200)
+        }
 
+        return render(self.request, 'plugins/gitlab_import_form.html', context, status=200)
+
+    def process_form_data(self, form_data):
+        other_repo_check  = form_data['other_repo_check']
+        if other_repo_check:
+            repo = quote(form_data['other_repo'].replace(self.gitlab_url, '').strip('/'), safe='')
+        else:
+            repo = quote(form_data['repo'].replace(self.gitlab_url, '').strip('/'), safe='')
+
+        url = '{api_url}/projects/{repo}/repository/files/{path}?ref={ref}'.format(
+            api_url=self.api_url,
+            repo=repo,
+            path=quote(form_data['path'], safe=''),
+            ref=quote(form_data['ref'], safe='')
+        )
+
+        return url
+    
     def get_success(self, request, response):
         file_content = response.json().get('content')
         request.session['import_file_name'] = handle_fetched_file(base64.b64decode(file_content))
