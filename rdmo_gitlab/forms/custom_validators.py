0c4be0b5b608290b2f6d937e57e62118b63b56c6
Laura Bahamon <bahamon@mpdl.mpg.de>
2026-03-12 16:12:01 +0100

    move validate_text_field and validate_(export_)file_path to rdmo_maus

--- rdmo_gitlab/forms/custom_validators.py
+++ rdmo_gitlab/forms/custom_validators.py
@@ -3,49 +3,7 @@ import re
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
-def validate_text_field(field_name, value, min_length, max_length, not_allowed_pattern, allowed_char_name_str):
-    errors = []
-    
-    matches = re.findall(not_allowed_pattern, value)
-    matches = list(set(matches))
-    if len(matches) > 0:
-        try:
-            index = matches.index(' ')
-            matches[index] = str(_('whitespace'))
-        except:
-            pass
-
-        errors.append(ValidationError(
-            _('{field_name} contains special character(s): "{spec_chars}". Allowed characters are: {allowed_char_name_str}.').format(
-                field_name=field_name,
-                spec_chars='", "'.join(matches),
-                allowed_char_name_str=allowed_char_name_str
-            ),
-            code='invalid'
-        ))
-    
-    if len(value) > max_length:
-        errors.append(ValidationError(
-            _('{field_name} must have at most {max_length} characters (it has {len_value}).').format(
-                field_name=field_name,
-                max_length=max_length,
-                len_value=len(value)
-            ),
-            code='invalid'
-        ))
-
-    if len(value) < min_length:
-        errors.append(ValidationError(
-            _('{field_name} must have at least {min_length} characters (it has {len_value}).').format(
-                field_name=field_name,
-                min_length=min_length,
-                len_value=len(value)
-            ),
-            code='invalid'
-        ))
-
-    if len(errors) > 0:
-        raise ValidationError(errors)
+from rdmo_maus.forms.custom_validators import validate_text_field
 
 def validate_new_repo_name(value):
     field_name = _('Repository name')
@@ -59,7 +17,7 @@ def validate_new_repo_name(value):
     try:
         validate_text_field(field_name, value, min_length, max_length, not_allowed_pattern, allowed_char_name_str)
     except ValidationError as ee:
-        errors.extend(e for e in ee.error_list if e not in errors)
+        errors.extend([e for e in ee.error_list if e not in errors])
 
     if value.startswith(('-', '_', '.', '+')):
         errors.append(ValidationError(
@@ -79,15 +37,6 @@ def validate_new_repo_name(value):
 
     if len(errors) > 0:
         raise ValidationError(errors)
-
-def validate_export_file_path(value):
-    field_name = _('File path')
-    min_length = 6
-    max_length = 100
-    not_allowed_pattern = f'[^A-Za-z0-9\/\-\_\.]'
-    allowed_char_name_str = _('alphanumeric, slash, hyphen, underscore, and period')
-
-    return validate_text_field(field_name, value, min_length, max_length, not_allowed_pattern, allowed_char_name_str)
 
 def validate_import_file_path(value):
     if not value.endswith('.xml'):
