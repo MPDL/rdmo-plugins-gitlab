43be40e8f232888621cd85e8e8220a6644870ed2
Laura Bahamon <bahamon@mpdl.mpg.de>
2026-05-08 15:09:27 +0200

    change indentation to 2 spaces, change some variable names

--- rdmo_gitlab/static/plugins/js/gitlab_form.js
+++ rdmo_gitlab/static/plugins/js/gitlab_form.js
@@ -1,37 +1,36 @@
-const repoToggleId = document.currentScript.getAttribute('repoToggleId')
-const checkedCollectionClass = document.currentScript.getAttribute('checkedCollectionClass')
-const uncheckedCollectionClass = document.currentScript.getAttribute('uncheckedCollectionClass')
+const cbId = document.currentScript.getAttribute('cbId')
+const checkedClass = document.currentScript.getAttribute('checkedClass')
+const uncheckedClass = document.currentScript.getAttribute('uncheckedClass')
 
 document.addEventListener('DOMContentLoaded', () => {
-    if (repoToggleId && checkedCollectionClass && uncheckedCollectionClass) {
-        toggleRepoFields(repoToggleId, checkedCollectionClass, uncheckedCollectionClass)
-    }
+  if (cbId && checkedClass && uncheckedClass) {
+    toggleRepoFields(cbId, checkedClass, uncheckedClass)
+  }
 })
 
+function toggleRepoFields(cbId, checkedClass, uncheckedClass) {
+  const checkBox = document.getElementById(cbId)
+  let checkedCollection = document.getElementsByClassName(checkedClass)
+  let uncheckedCollection = document.getElementsByClassName(uncheckedClass)
 
-function toggleRepoFields(checkbox_id, checked_collection_class, unchecked_collection_class) {
-    const checkBox = document.getElementById(checkbox_id)
-    var checkedCollection = document.getElementsByClassName(checked_collection_class)
-    var uncheckedCollection = document.getElementsByClassName(unchecked_collection_class)
-
-    if (checkBox.checked == true){
-        checkedCollection[0].style.display = 'block'
-        uncheckedCollection[0].style.display = 'none'
-    } else {
-        checkedCollection[0].style.display = 'none'
-        uncheckedCollection[0].style.display = 'block'
-    }
+  if (checkBox.checked == true){
+    checkedCollection[0].style.display = 'block'
+    uncheckedCollection[0].style.display = 'none'
+  } else {
+    checkedCollection[0].style.display = 'none'
+    uncheckedCollection[0].style.display = 'block'
+  }
 }
 
-function hideAllChoiceWarningMessages(text, choice_count) {
-    var duration = 1000
-    clearTimeout(text._timer)
-    text._timer = setTimeout(()=>{
-        for (let i=0; i<choice_count; i++) {
-            let choice_warning_messages = document.getElementById(`id_warnings_${i}`)
-            if (choice_warning_messages) {
-                choice_warning_messages.style.display = 'none'
-            }
-        }
-    }, duration)
+function hideAllChoiceWarningMessages(text, choiceCount) {
+  var duration = 1000
+  clearTimeout(text._timer)
+  text._timer = setTimeout(()=>{
+    for (let i=0; i<choiceCount; i++) {
+      let choiceWarningMessages = document.getElementById(`id_warnings_${i}`)
+      if (choiceWarningMessages) {
+        choiceWarningMessages.style.display = 'none'
+      }
+    }
+  }, duration)
 }
